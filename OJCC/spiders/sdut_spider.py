5381110e5349930f1efc06a7a8a5b970c9fdf1cc
nplus <junnplus@qq.com>
2015-06-30 02:52:22 +0800

    update sdut spider

--- OJCC/spiders/sdut_spider.py
+++ OJCC/spiders/sdut_spider.py
@@ -1,21 +1,25 @@
 from scrapy.spider import Spider
+from scrapy.selector import Selector
 from OJCC.items import ProblemItem
 
 class SdutSpider(Spider):
     name = 'sdut'
     allowed_domains = ['acm.sdut.edu.cn']
     start_urls = [
-        'http://acm.sdut.edu.cn/sdutoj/problem.php?action=showproblem&problemid=1018'
+        'http://acm.sdut.edu.cn/sdutoj/problem.php?action=showproblem&problemid=1018',
+        'http://acm.sdut.edu.cn/sdutoj/problem.php?action=showproblem&problemid=1017'
         ]
 
     def parse(self, response):
+        sel = Selector(response)
+
         item = ProblemItem()
-        item['title'] = response.xpath('//center/h2').extract()[0]
-        item['description'] = response.css('.pro_desc').extract()[0]
-        item['input'] = response.css('.pro_desc').extract()[1]
-        item['output'] = response.css('.pro_desc').extract()[2]
-        item['time_limit'] = response.xpath('//a/h5/text()').re('T[\S*\s]*s')[0]
-        item['memory_limit'] = response.xpath('//a/h5/text()').re('M[\S*\s]*K')[0]
-        item['sample_input'] = response.xpath('//pre').extract()[0]
-        item['sample_output'] = response.xpath('//pre').extract()[1]
+        item['title'] = sel.xpath('//center/h2/text()').extract()[0]
+        item['description'] = sel.css('.pro_desc').extract()[0]
+        item['input'] = sel.css('.pro_desc').extract()[1]
+        item['output'] = sel.css('.pro_desc').extract()[2]
+        item['time_limit'] = sel.xpath('//a/h5/text()').re('T[\S*\s]*s')[0]
+        item['memory_limit'] = sel.xpath('//a/h5/text()').re('M[\S*\s]*K')[0]
+        item['sample_input'] = sel.xpath('//pre').extract()[0]
+        item['sample_output'] = sel.xpath('//pre').extract()[1]
         yield item
