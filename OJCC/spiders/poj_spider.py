41af6dd414af5527220c0403f3a8f8f806ab70cc
nplus <junnplus@qq.com>
2015-07-02 02:02:19 +0800

    add poj sumbit spider

--- OJCC/spiders/poj_spider.py
+++ OJCC/spiders/poj_spider.py
@@ -1,12 +1,13 @@
 from scrapy.spider import Spider
+from scrapy.http import Request, FormRequest
 from scrapy.selector import Selector
 from OJCC.items import ProblemItem
 
-class PojSpider(Spider):
-    name = 'poj'
+class PojProblemSpider(Spider):
+    name = 'poj_problem'
     allowed_domains = ['poj.org']
 
-    def __init__(self, problem_id=1000, *args, **kwargs):
+    def __init__(self, problem_id='1000', *args, **kwargs):
         super(PojSpider, self).__init__(*args, **kwargs)
         self.start_urls = [
             'http://poj.org/problem?id=%s' % problem_id
@@ -27,3 +28,60 @@ class PojSpider(Spider):
         item['sample_input'] = sel.css('.sio').extract()[0]
         item['sample_output'] = sel.css('.sio').extract()[1]
         yield item
+
+class PojSubmitSpider(Spider):
+    name = 'poj_submit'
+    allowed_domains = ['poj.org']
+    login_url = 'http://poj.org/login'
+    submit_url = 'http://poj.org/submit'
+    source = \
+        'I2luY2x1ZGUgPHN0ZGlvLmg+CgppbnQgbWFpbigpCnsKICAgIGludCBhLGI7CiAgICBzY2FuZigiJWQgJWQiLCZhLCAmYik7CiAgICBwcmludGYoIiVkXG4iLGErYik7CiAgICByZXR1cm4gMDsKfQ=='
+
+    start_urls = [
+        "http://poj.org/status"
+    ]
+
+    def __init__(self, problem_id='1000', language='0', source=None, *args, **kwargs):
+        self.problem_id = problem_id
+        self.language = language
+        if source is not None:
+            self.source = source
+
+    headers = {
+        "Accept": "*/*",
+        "Accept-Encoding": "gzip,deflate",
+        "Accept-Language": "en-US,en;q=0.8,zh-TW;q=0.6,zh;q=0.4",
+        "Connection": "keep-alive",
+        "Content-Type":" application/x-www-form-urlencoded; charset=UTF-8",
+        "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/38.0.2125.111 Safari/537.36",
+    }
+
+    def start_requests(self):
+        return [FormRequest(self.login_url,
+                headers = self.headers,
+                formdata = {
+                        'user_id1': 'sdutacm1',
+                        'password1': 'sdutacm',
+                        'B1': 'login',
+                },
+                callback = self.after_login,
+                dont_filter = True
+        )]
+
+    def after_login(self, response):
+        return [FormRequest(self.submit_url,
+                headers = self.headers,
+                formdata = {
+                        'problem_id': self.problem_id,
+                        'language': self.language,
+                        'source': self.source,
+                        'submit': 'Submit',
+                        'encoded': '1'
+                },
+                callback = self.after_submit,
+                dont_filter = True
+        )]
+
+    def after_submit(self, response):
+        for url in self.start_urls :
+            yield self.make_requests_from_url(url)
