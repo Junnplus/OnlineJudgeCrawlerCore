897a9b179ebf90a3a6a362eaabd9eb4771d69ec4
nplus <junnplus@qq.com>
2015-07-02 02:02:28 +0800

    add hdu sumbit spider

--- OJCC/spiders/hdu_spider.py
+++ OJCC/spiders/hdu_spider.py
@@ -1,12 +1,13 @@
 from scrapy.spider import Spider
+from scrapy.http import Request, FormRequest
 from scrapy.selector import Selector
 from OJCC.items import ProblemItem
 
-class HduSpider(Spider):
-    name = 'hdu'
+class HduProblemSpider(Spider):
+    name = 'hdu_problem'
     allowed_domains = ['acm.hdu.edu.cn']
 
-    def __init__(self, problem_id=1000, *args, **kwargs):
+    def __init__(self, problem_id='1000', *args, **kwargs):
         super(HduSpider, self).__init__(*args, **kwargs)
         self.start_urls = [
             'http://acm.hdu.edu.cn/showproblem.php?pid=%s' % problem_id
@@ -27,3 +28,59 @@ class HduSpider(Spider):
         item['sample_input'] = sel.xpath('//pre').extract()[0]
         item['sample_output'] = sel.xpath('//pre').extract()[1]
         yield item
+
+class HduSubmitSpider(Spider):
+    name = 'hdu_submit'
+    allowed_domains = ['acm.hdu.edu.cn']
+    login_url = 'http://acm.hdu.edu.cn/userloginex.php?action=login'
+    submit_url = 'http://acm.hdu.edu.cn/submit.php?action=submit'
+    source = \
+        'I2luY2x1ZGUgPHN0ZGlvLmg+CgppbnQgbWFpbigpCnsKICAgIGludCBhLGI7CiAgICBzY2FuZigiJWQgJWQiLCZhLCAmYik7CiAgICBwcmludGYoIiVkXG4iLGErYik7CiAgICByZXR1cm4gMDsKfQ=='
+
+    start_urls = [
+        'http://acm.hdu.edu.cn/status.php'
+    ]
+
+    def __init__(self, problem_id='1000', language='0', source=None, *args, **kwargs):
+        self.problem_id = problem_id
+        self.language = language
+        if source is not None:
+            self.source = source
+
+    headers = {
+        "Accept": "*/*",
+        "Accept-Encoding": "gzip,deflate",
+        "Accept-Language": "en-US,en;q=0.8,zh-TW;q=0.6,zh;q=0.4",
+        "Connection": "keep-alive",
+        "Content-Type":" application/x-www-form-urlencoded; charset=UTF-8",
+        "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/38.0.2125.111 Safari/537.36",
+    }
+
+    def start_requests(self):
+        return [FormRequest(self.login_url,
+                headers = self.headers,
+                formdata = {
+                        'username': 'sdutacm1',
+                        'userpass': 'sdutacm',
+                        'login': 'Sign+In',
+                },
+                callback = self.after_login,
+                dont_filter = True
+        )]
+
+    def after_login(self, response):
+        return [FormRequest(self.submit_url,
+                headers = self.headers,
+                formdata = {
+                        'problemid': self.problem_id,
+                        'language': self.language,
+                        'usercode': self.source,
+                        'check': '0'
+                },
+                callback = self.after_submit,
+                dont_filter = True
+        )]
+
+    def after_submit(self, response):
+        for url in self.start_urls :
+            yield self.make_requests_from_url(url)
