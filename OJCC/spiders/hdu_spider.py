d78d4c4aa48fb2f61d28bd402965ba84e2c148dd
nplus <junnplus@qq.com>
2015-07-02 17:17:59 +0800

    update warning

--- OJCC/spiders/hdu_spider.py
+++ OJCC/spiders/hdu_spider.py
@@ -1,4 +1,4 @@
-from scrapy.spider import Spider
+from scrapy.spiders import Spider
 from scrapy.http import Request, FormRequest
 from scrapy.selector import Selector
 from OJCC.items import ProblemItem
