a7ba03af7c2601cd72e4081c4b03d6d4ea1e6ebd
nplus <junnplus@qq.com>
2015-07-17 00:04:02 +0800

    update

--- OJCC/spiders/sdut_spider.py
+++ OJCC/spiders/sdut_spider.py
@@ -195,8 +195,7 @@ class SdutSubmitSpider(CrawlSpider):
                     item['time'] = \
                         tr.xpath('.//td')[5].xpath('./text()').extract()[0]
                 except:
-                    item['memory'] = []
-                    item['time'] = []
+                    pass
 
                 item['code_length'] = tr.xpath('.//td/text()').extract()[-2]
                 item['result'] = tr.xpath('.//td').xpath('.//font/text()').extract()[0]
